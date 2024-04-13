e4097c85c5e01ab24f44e7c05891f28f243010e2
Gilsabo <gilsala_88@hotmail.com>
2024-04-13 10:39:27 +0200

    Add function to fetch ships and render them

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+
+interface Starship {
+  name: string;
+}
 
 export default function App() {
-  return <h1>hi!</h1>;
+  const [ships, setShips] = useState<Starship[]>();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://swapi.py4e.com/api/starships/?page=1',
+        );
+        const shipsInfo = await response.json();
+        setShips(shipsInfo.results);
+        console.log('ships', shipsInfo);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log(ships);
+  return (
+    <div>
+      <h1>hi!</h1>
+      <h2>Starhsips</h2>
+      {ships?.map((ship) => <div key={`div-${ship.name}`}>{ship.name}</div>)}
+    </div>
+  );
 }
