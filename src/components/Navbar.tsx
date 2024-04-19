c139c3255cc766656656bd748568596d955fae21
Gilsabo <gilsala_88@hotmail.com>
2024-04-19 09:03:43 +0200

    Add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">HOME</Link>
+        </li>
+        <li>
+          <Link to="starships">STARSHIPS</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
