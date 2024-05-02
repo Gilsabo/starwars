e1b858aa785a90e5b5aee82c2cb7674967ec221a
Gilsabo <gilsala_88@hotmail.com>
2024-05-02 20:39:24 +0200

    Pass props films

--- src/components/Films.tsx
+++ src/components/Films.tsx
@@ -1,3 +1,14 @@
-export default function Films() {
-  return <div>Films</div>;
+type Props = {
+  films: string[];
+};
+
+export default function Films(props: Props) {
+  return (
+    <div>
+      <div>films</div>
+      {props.films.map((film) => (
+        <div key={`div${film}`}>{film}</div>
+      ))}
+    </div>
+  );
 }
