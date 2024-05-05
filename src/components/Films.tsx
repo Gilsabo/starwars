b8dc5343c5ffe998922026a4fe5377fd95d16d79
Gilsabo <gilsala_88@hotmail.com>
2024-05-05 17:04:51 +0200

    Add function to get url id from films

--- src/components/Films.tsx
+++ src/components/Films.tsx
@@ -3,6 +3,13 @@ type Props = {
 };
 
 export default function Films(props: Props) {
+  const filmsUrlIds = props.films.map((film) => {
+    const extractId = film.split('/');
+    return extractId[extractId.length - 2];
+  });
+
+  console.log(filmsUrlIds);
+
   return (
     <div>
       <div>films</div>
