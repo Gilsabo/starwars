6913cbd9103613975a311d3b8b262a92a704d540
Gilsabo <gilsala_88@hotmail.com>
2024-05-13 11:02:16 +0200

    Add fetch function pilots names

--- src/components/Pilots.tsx
+++ src/components/Pilots.tsx
@@ -1,12 +1,39 @@
+import { useQueries } from '@tanstack/react-query';
+import { getPilotName } from '../utils/apis';
+
 type Props = {
   pilots: string[];
 };
 
 export default function Pilots(props: Props) {
+  const pilotsUrlIds = props.pilots.map((pilot) => {
+    const extractId = pilot.split('/');
+    return extractId[extractId.length - 2];
+  });
+
+  console.log(pilotsUrlIds, props.pilots);
+
+  const fetchPilotsNames = useQueries({
+    queries: pilotsUrlIds.map((pilotsId, index) => {
+      return {
+        queryKey: ['user', pilotsId],
+        queryFn: () => getPilotName(pilotsUrlIds[index]),
+      };
+    }),
+  });
+  console.log(fetchPilotsNames);
   return (
     <>
       <div>Pilots</div>
-      <div>{props.pilots}</div>
+      {fetchPilotsNames.length !== 0 ? (
+        fetchPilotsNames.map((pilotName) => (
+          <div key={`div${pilotName.data?.name as string}`}>
+            {pilotName.data?.name}
+          </div>
+        ))
+      ) : (
+        <div>no pilots</div>
+      )}
     </>
   );
 }
