4019255959e0c61733e79d6f3f0e41e68ddb4560
Gilsabo <gilsala_88@hotmail.com>
2024-05-18 05:09:00 +0200

    Add pagination

--- src/components/StarshipsLoader.tsx
+++ src/components/StarshipsLoader.tsx
@@ -1,35 +1,51 @@
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Starship } from '../App';
 import { getStarships } from '../utils/apis';
 
 export default function Starships() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ['starshipsData'],
-    queryFn: () => getStarships(),
-  });
+  const [pagination, setPagination] = useState(1);
+  // const { isPending, error, data } = useQuery({
+  //   queryKey: ['starshipsData'],
+  //   queryFn: () => getStarships(),
+  // });
 
-  if (isPending) return 'Loading...';
-  if (error) return 'An error has occurred' + error.message;
+  // if (isPending) return 'Loading...';
+  // if (error) return 'An error has occurred' + error.message;
 
-  const starshipUrlIds: string = data.results.map((ship: Starship) => {
-    const extractId = ship.url.split('/');
-    return extractId[extractId.length - 2];
-  });
+  // const starshipUrlIds: string = data.results.map((ship: Starship) => {
+  //   const extractId = ship.url.split('/');
+  //   return extractId[extractId.length - 2];
+  // });
+
+  // return (
+  //   <main>
+  //     <section>
+  //       <ul>
+  //         {data.results.map((starship: Starship, index: number) => (
+  //           <Link key={`div-${starship.name}`} to={`${starshipUrlIds[index]}`}>
+  //             <li>{starship.name}</li>
+  //             <li>{starship.model}</li>
+  //           </Link>
+  //         ))}
+  //       </ul>
+  //       <button> view more</button>
+  //     </section>
+  //   </main>
+  // );
+
+  const getStarshipsWithPagination = async () => {
+    const res = await fetch(
+      `https://swapi.dev/api/starships/?page=${pagination}`,
+    );
+    return res.json();
+  };
 
   return (
-    <main>
-      <section>
-        <ul>
-          {data.results.map((starship: Starship, index: number) => (
-            <Link key={`div-${starship.name}`} to={`${starshipUrlIds[index]}`}>
-              <li>{starship.name}</li>
-              <li>{starship.model}</li>
-            </Link>
-          ))}
-        </ul>
-        <button> view more</button>
-      </section>
-    </main>
+    <>
+      <div>proof</div>
+      <button onClick={() => setPagination(pagination + 1)}>view more</button>
+    </>
   );
 }
