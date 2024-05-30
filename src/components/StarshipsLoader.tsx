74e7942c533e1c3d8cb038b51b723225e3395d87
Gilsabo <gilsala_88@hotmail.com>
2024-05-30 09:45:29 +0200

    Add links

--- src/components/StarshipsLoader.tsx
+++ src/components/StarshipsLoader.tsx
@@ -1,7 +1,6 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { Link } from 'react-router-dom';
 import { Starship } from '../App';
-
-// import { Link } from 'react-router-dom';
 
 type Page = {
   name: string;
@@ -40,6 +39,15 @@ export default function Starships() {
   console.log(data?.pages);
   console.log(data?.pageParams, 'params');
 
+  const starshipUrlIds: string[] =
+    data?.pages.flatMap((page) =>
+      page.results.map((ship: Starship) => {
+        const extractId = ship.url.split('/');
+        return extractId[extractId.length - 2];
+      }),
+    ) || [];
+
+  console.log('dd', starshipUrlIds);
   return status === 'pending' ? (
     <p>Loading...</p>
   ) : status === 'error' ? (
@@ -48,10 +56,11 @@ export default function Starships() {
     <>
       <ul>
         {data.pages.map((page) =>
-          page.results.map((starship: Starship) => (
-            <div key={`div-starship-${starship.name}`}>
-              {starship.name} - {starship.model}
-            </div>
+          page.results.map((starship: Starship, index: number) => (
+            <Link key={`div-${starship.name}`} to={`${starshipUrlIds[index]}`}>
+              <li>{starship.name}</li>
+              <li>{starship.model}</li>
+            </Link>
           )),
         )}
       </ul>
