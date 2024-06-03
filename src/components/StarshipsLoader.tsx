bfeca118cff6340767b1925c061b77f2dc961e15
Gilsabo <gilsala_88@hotmail.com>
2024-06-03 10:12:33 +0200

    Refactor code

--- src/components/StarshipsLoader.tsx
+++ src/components/StarshipsLoader.tsx
@@ -1,21 +1,9 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 import { Starship } from '../App';
+import { getStarshipsWithPagination } from '../utils/apis';
 
 export default function Starships() {
-  const getStarshipsWithPagination = async (pageParam: number) => {
-    const response = await fetch(
-      `https://swapi.dev/api/starships/?page=${pageParam}`,
-    );
-    const data = await response.json();
-
-    return {
-      nextPage: data.next ? pageParam + 1 : undefined,
-      previousPage: data.previous ? pageParam - 1 : undefined,
-      results: data.results,
-    };
-  };
-
   const {
     data,
     error,
