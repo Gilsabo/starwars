ab92497a64177f3c6bde26175c581a6a8bf58f73
Gilsabo <gilsala_88@hotmail.com>
2024-06-04 10:47:55 +0200

    Add login and signup components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Login from './components/Login';
 import Navbar from './components/Navbar';
+import SignUp from './components/SignUp';
 import StarshipCard from './components/StarshipCard';
 import StarshipsLoader from './components/StarshipsLoader';
 
@@ -20,6 +22,8 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
         <Route path="starships" element={<StarshipsLoader />} />
         <Route path="starships/:id" element={<StarshipCard />} />
       </Routes>
