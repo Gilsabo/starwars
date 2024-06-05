742ecfe91872121eda9a3a8f3bcb20615dbc1a22
Gilsabo <gilsala_88@hotmail.com>
2024-06-05 11:25:42 +0200

    Add component login

---
+++ src/components/Login.tsx
@@ -0,0 +1,17 @@
+export default function Login() {
+  return (
+    <>
+      <h2>Log in</h2>
+      <form action="">
+        <label>
+          User name
+          <input />
+        </label>
+        <label>
+          E-mail
+          <input />
+        </label>
+      </form>
+    </>
+  );
+}
