5b11129a86205c23b258e405bba868a7bf1764f5
Gilsabo <gilsala_88@hotmail.com>
2024-06-08 07:09:10 +0200

    Add form

---
+++ src/components/SignUp.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function SignUp() {
+  return (
+    <>
+      <div>Sign up</div>
+      <form action="">
+        <label>
+          User name
+          <input />
+        </label>
+        <label>
+          E-mail
+          <input />
+        </label>
+      </form>
+    </>
+  );
+}
