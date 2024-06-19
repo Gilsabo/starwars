bb7df2bb333b2494c57c12c6445ce523e52abbf6
Gilsabo <gilsala_88@hotmail.com>
2024-06-19 09:49:07 +0200

    Add function handleSubmit and handleChange

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 export default function SignUp() {
-  return (
-    <>
-      <div>Sign up</div>
-      <form action="">
-        <label>
-          User name
-          <input />
-        </label>
-        <label>
-          E-mail
-          <input />
-        </label>
-      </form>
-    </>
-  );
+  const [formData, setFormData] = useState({
+    email: '', // required
+    password: '', // required
+    username: '', // optional
+  });
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    return (
+      <>
+        <div>Sign up</div>
+        <form action="">
+          <label>
+            User name
+            <input />
+          </label>
+          <label>
+            E-mail
+            <input />
+          </label>
+        </form>
+      </>
+    );
+  }
 }
