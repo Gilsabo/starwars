f2a08e071a77de4aed6263629e4e56d4ec01099d
Gilsabo <gilsala_88@hotmail.com>
2024-06-25 09:32:56 +0200

    Add form with event handlers

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,17 +1,44 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '', // required
+    password: '', // required
+  });
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data.user));
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
   return (
-    <>
-      <h2>Log in</h2>
-      <form action="">
-        <label>
-          User name
-          <input />
-        </label>
-        <label>
-          E-mail
-          <input />
-        </label>
+    <div>
+      <h1>Login Form</h1>
+      <form className="login-form" onSubmit={(e) => handleSubmit(e)}>
+        <input
+          placeholder="Email"
+          value={formData.email}
+          name="email"
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          placeholder="Password"
+          value={formData.password}
+          name="password"
+          onChange={(e) => handleChange(e)}
+        />
+        <button className="login-btn">Login</button>
       </form>
-    </>
+    </div>
   );
 }
